565d493148eebadedb5e9a5fe7d333a38af14617
Leandro Ferraz <leandro_ferraz@outlook.com>
2023-02-09 21:43:01 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function calcularPlano() {
     const salario = Number(document.getElementById("salario").value);
     let resultado;
 
-    if (salario > 3000) 
+    if (salario > 3000)
         resultado = "Oferecer Plano Alfa"
     else {
         resultado = "Oferecer Plano Beta";
@@ -15,7 +15,7 @@ function calcularColesterol() {
     const colesterol = Number(document.getElementById("colesterol").value);
     let resultado;
 
-    if (colesterol < 180) 
+    if (colesterol < 180)
         resultado = "Saúde Ok"
     else {
         resultado = "Vamos procurar uma ajuda médica?";
@@ -28,11 +28,38 @@ function calcularComissao() {
     const totalVenda = Number(document.getElementById("totalVenda").value);
     let resultado;
 
-    if (totalVenda >= 100000) 
+    if (totalVenda >= 100000)
         resultado = totalVenda * 0.04
     else {
         resultado = totalVenda * 0.02
     }
 
     document.getElementById("resultadoComissao").innerHTML = resultado;
+}
+
+function calcularNota() {
+    const nota = Number(document.getElementById("nota").value);
+    let resultado;
+
+    if (nota < 0 || nota > 10)
+        resultado = "Nota inválida"
+    else if (nota >= 0 && nota < 4)
+        resultado = "Aluno reprovado!"
+    else if (nota >= 4 && nota < 7) {
+        resultado = "Aluno em recuperação!"
+    } else
+        resultado = "Aluno aprovado!"
+
+    document.getElementById("resultadoNota").innerHTML = resultado;
+}
+
+function recuperarNota() {
+    const nota = Number(document.getElementById("nota").value);
+    const notaRecuperacao = Number(document.getElementById("recuperacaoNota").value);
+    let resultado = nota + notaRecuperacao;
+
+    if (resultado > 5)
+        document.getElementById("resultadoRecuperacao").innerHTML = "Aprovado na recuperação";
+    else
+        document.getElementById("resultadoRecuperacao").innerHTML = "Reprovado na recuperação";
 }
